73cd38e4ae1e29f11b33035ea40cafd75d49e711
matsuoyuta <y.matsuo.dev@gmail.com>
2022-01-03 11:27:22 +0900

    Add husky for checking swagger diffs at pre-commit

---
+++ packages/server/src/tasks/checkOpenAPIDocument.ts
@@ -0,0 +1,51 @@
+import { SwaggerModule } from '@nestjs/swagger';
+import { Test } from '@nestjs/testing';
+import { swaggerConfig } from '../configs/swagger';
+import { AppModule } from '../app.module';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs-extra';
+import * as path from 'path';
+
+const load = (filename: string) => {
+  const yamlText = fs.readFileSync(filename, 'utf8');
+  return yaml.load(yamlText);
+};
+
+const dump = (object: any) => {
+  return yaml.dump(object, {
+    skipInvalid: true,
+    noRefs: true,
+  });
+};
+
+const bootstrap = async (): Promise<void> => {
+  const modules = Reflect.getMetadata('imports', AppModule);
+  const controllers = Reflect.getMetadata('controllers', AppModule);
+  const providers = Reflect.getMetadata('providers', AppModule);
+
+  const testingModule = await Test.createTestingModule({
+    imports: modules,
+    controllers: controllers,
+    providers: providers,
+  }).compile();
+
+  const app = await testingModule.createNestApplication();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  const newDump = dump(document);
+
+  const yamlPath = path.join('..', 'swagger/swagger.yml');
+  const origin = load(yamlPath);
+  const originDump = dump(origin);
+
+  console.log('origin:', originDump);
+  console.log('new:', newDump);
+
+  if (originDump === newDump) {
+    console.log('SAME!!');
+    process.exit(0);
+  } else {
+    console.log('DIFFER!!');
+    process.exit(1);
+  }
+};
+bootstrap();
