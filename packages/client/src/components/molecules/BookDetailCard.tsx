0c2257ce7f4d0b27d5d88ae53ed884e47b7a69c1
matsuoyuta <y.matsuo.dev@gmail.com>
2022-01-06 21:30:11 +0900

    Fix BookDetailCard to display markdown components

--- packages/client/src/components/molecules/BookDetailCard.tsx
+++ packages/client/src/components/molecules/BookDetailCard.tsx
@@ -1,9 +1,12 @@
-import dayjs from 'dayjs';
 import { css } from '@emotion/react';
 import { Book } from '../../api/generated';
 import { StatusLabel } from '../atoms/StatusLabel';
 import { dateText } from '../../utils/dateUtil';
 import { fontSize } from '../../styles/fontSize';
+import * as React from 'react';
+import { convertMarkdownToHTML } from '../../utils/sanitize';
+import { TextareaForm } from '../atoms/TextareaForm';
+import { MarkdownAndHTMLArea } from './MarkdownAndHTMLArea';
 
 type Props = {
   book: Book;
@@ -26,16 +29,27 @@ const bookTitle = css`
 `;
 
 export const BookDetailCard = ({ book }: Props) => {
+  const [markdown, setMarkDown] = React.useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkDown(e.target.value);
+  };
+
   return (
-    <ul css={table}>
-      <li>
-        <img css={image} src={book?.image_path} />
-      </li>
-      <li css={bookTitle}>{book?.name}</li>
-      <li>{book?.author}</li>
-      <li>{book?.publisher}</li>
-      <li>{StatusLabel(book?.status)}</li>
-      <li>{dateText(book?.createdAt)}</li>
-    </ul>
+    <>
+      <ul css={table}>
+        <li>
+          <img css={image} src={book?.image_path} />
+        </li>
+        <li css={bookTitle}>{book?.name}</li>
+        <li>{book?.author}</li>
+        <li>{book?.publisher}</li>
+        <li>{StatusLabel(book?.status)}</li>
+        <li>{dateText(book?.createdAt)}</li>
+      </ul>
+      <div>
+        <MarkdownAndHTMLArea value={markdown} onChange={handleChange} />
+      </div>
+    </>
   );
 };
