001785ca3995e57cae874e22d67bf3de72b916f3
matsuoyuta <y.matsuo.dev@gmail.com>
2022-01-27 07:50:20 +0900

    Add production domain in swagger

--- packages/server/src/configs/swagger.ts
+++ packages/server/src/configs/swagger.ts
@@ -5,4 +5,5 @@ export const swaggerConfig = new DocumentBuilder()
   .setDescription('API Docs')
   .setVersion('1.0')
   .addServer('localhost:3000/api', 'local')
+  .addServer('api.bookshelf.yu-ta-9.com/api', 'production')
   .build();
