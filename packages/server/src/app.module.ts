3a32a8adc7f8cb2ef012a8022b11f66e519a0aae
matsuoyuta <y.matsuo.dev@gmail.com>
2022-01-30 12:05:11 +0900

    Change ormconfig to switch some environments

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { ormconfig } from './configs/ormconfig';
 
 // TODO: CORS設定を付加する
 @Module({
@@ -12,22 +13,13 @@ import { BooksModule } from './books/books.module';
       isGlobal: true,
       envFilePath: process.env.NODE_ENV === 'production' ? '' : '.env',
       ignoreEnvFile: process.env.NODE_ENV === 'production' ? true : false,
+      load: [ormconfig],
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USER_NAME,
-      password: process.env.DB_PASSWORD,
-      database: 'reading_management',
-      entities: ['./dist/**/*.entity{.ts,.js}'],
-      migrations: ['./dist/src/migrations/*{.ts,.js}'],
-      synchronize: false,
-      connectTimeout: 30 * 1000,
-      logging: true,
-      cli: {
-        entitiesDir: './dist/src',
-        migrationsDir: './src/migrations',
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return configService.get('database');
       },
     }),
     BooksModule,
---
+++ packages/server/src/configs/ormconfig.ts
@@ -0,0 +1,24 @@
+/**
+ * MEMO: appとmigration時どちらからも共通の設定を参照できるようにするために用意
+ */
+export const ormconfig = () => ({
+  database: {
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_PASSWORD,
+    database: 'bookshelf',
+    entities: ['./dist/**/*.entity{.ts,.js}'],
+    migrations: ['./dist/src/migrations/*{.ts,.js}'],
+    synchronize: false,
+    connectTimeout: 30 * 1000,
+    logging: true,
+    cli: {
+      entitiesDir: './dist/src',
+      migrationsDir: './src/migrations',
+    },
+  },
+});
+
+export default ormconfig().database;
